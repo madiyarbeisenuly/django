87acbf1187df62a934c35a8fb82d24b662296484
urfikk <gamma.laser@mail.ru>
2025-03-27 12:17:48 +0000

    Удалил некорректные файлы и папки

--- myproject1/myproject1/myproject1/settings.py
+++ myproject1/myproject1/myproject1/settings.py
@@ -113,12 +113,22 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from pathlib import Path
+import os
+
+# Определяем корневую директорию проекта
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Настраиваем путь к статическим файлам
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
